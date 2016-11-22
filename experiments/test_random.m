7320d9c4b5f3588d36578a9acc89d682a26a4813
Z Daniels <zad7@cs.rutgers.edu>
2016-11-22 09:37:44 -0500

    Working

--- experiments/test_random.m
+++ experiments/test_random.m
@@ -96,12 +96,30 @@ params.orthogonalConstraint = 'w';
 [W,H,D,F,FIters] = nmft(data,k,params);
 FItersAll(:,5) = FIters';
 
-colors = {'red','green','blue','black','cyan'};
+params = [];
+params.method = 'mult';
+params.maxIters = 100;
+params.initialization = 'nndsvd';
+params.loss = 'sqeuclidean';
+params.evalLoss = 'sqeuclidean';
+params.stepType = 'steepest';
+params.paramH = 0.5;
+params.paramW = 0.5;
+params.sparseParamH = 0.75;
+params.sparseParamW = 0.75;
+params.subIters = 1;
+params.printIter = true;
+params.orthogonalConstraint = 'w';
+
+[W,H,D,F,FIters] = nmft(data,k,params);
+FItersAll(:,6) = FIters';
+
+colors = {'red','green','blue','black','cyan','yellow'};
 
 close all;
 figure;
 hold on;
-for i = 1:1:5
+for i = 1:1:6
     plot(FItersAll(:,i),'Color',colors{i})
 end
 hold off;
